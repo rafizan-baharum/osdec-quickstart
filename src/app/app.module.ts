4acabc0553365597ed8dfe297019fe69260a60da
Rafizan Uda Baharum <rafizan.baharum@canang.com.my>
2017-12-03 15:43:25 +0800

    Initial checkin

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {SharedModule} from './shared/shared.module';
+import {AppRoutingModule, routedComponents} from './app.routes';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    routedComponents,
+  ], // directives, components, and pipes owned by this NgModule
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    SharedModule,
+  ], // modules needed to run this module
+  providers: [], // additional providers needed for this module
+  entryComponents: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {
+}
